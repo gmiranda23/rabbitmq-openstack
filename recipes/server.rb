491a8a2dab26d993e82bfacde7fe5b76f6e42861
mattray <matthewhray@gmail.com>
2012-10-02 10:44:46 -0500

    next rev of rabbitmq supports the distro version

--- recipes/server.rb
+++ recipes/server.rb
@@ -11,21 +11,6 @@ node.set["rabbitmq"]["address"] = rabbit_info["host"]
 #             we should probably setup different users for nova and glance
 # TODO(shep): Should probably use Opscode::OpenSSL::Password for default_password
 
-case node["platform"]
-# Since the upstream rabbitmq server does crazy things, like install packages from
-# random apt repos.. lets pin to the ubuntu repo.
-when "ubuntu", "debian"
-  apt_preference "rabbitmq-server" do
-    pin "release o=Ubuntu"
-    pin_priority "700"
-  end
-# TODO(darren) do we want packages from rabbit site, or epel/distro?
-# if the latter, then we can use the below to override the default in the
-# opscode rabbitmq cookbook
-#when "redhat", "centos", "fedora"
-#  node.override["rabbitmq"]["use_yum"] = true
-end
-
 include_recipe "rabbitmq::default"
 
 # TODO - this needs to be templated out
